1184a7ba807ffde968d6f5d075264548a563e2b8
tombrereton <tbrereton@gmail.com>
2018-02-14 22:49:56 +0000

    Update index file to test react router dom

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,17 +1,71 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./style.scss";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>It Works!</h1>
-        <p>
-          This React project just works including{" "}
-          <span className="redBg">module</span> local styles.
-        </p>
-        <p>Enjoy!</p>
-      </div>
-    );
-  }
-}
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/blog" component={Blog} />
+    </div>
+  </Router>
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const Projects = () => (
+  <div>
+    <h2>Projects</h2>
+  </div>
+);
+
+const Blog = ({ match }) => (
+  <div>
+    <h2>Blog</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
+export default BasicExample;
